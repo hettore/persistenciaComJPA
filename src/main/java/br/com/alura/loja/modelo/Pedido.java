474188b29b053ce7c742976f3645d1e17f6a5284
Hettore Eduardo <duh.barretoo@gmail.com>
2023-05-29 09:35:48 -0300

    Mapeando novas entidades

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,68 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido() {
+	}
+
+	public Pedido(Cliente cliente) {
+		super();
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
